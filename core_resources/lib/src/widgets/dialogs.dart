2708a830b79222eadad1eb8857588659c9a06ff5
Icaro Temponi <icarohs7@gmail.com>
2019-11-05 10:28:10 -0300

    Add showConfirmDialog top level function
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/widgets/dialogs.dart
+++ core_resources/lib/src/widgets/dialogs.dart
@@ -8,15 +8,47 @@ Future<T> showSimpleAlert<T>(
 }) {
   return showDialog(
     context: context,
-    builder: (context) => AlertDialog(
-      title: title,
-      content: content,
-      actions: <Widget>[
-        FlatButton(
-          child: Text("Ok"),
-          onPressed: () => onConfirm != null ? onConfirm() : Navigator.pop(context),
-        ),
-      ],
-    ),
+    builder: (context) {
+      return AlertDialog(
+        title: title,
+        content: content,
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Ok"),
+            onPressed: () => onConfirm != null ? onConfirm() : Navigator.pop(context),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<T> showConfirmDialog<T>(
+  BuildContext context, {
+  Widget title,
+  Widget content,
+  Function() onConfirm,
+  Function() onCancel,
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: title,
+        content: content,
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Cancelar"),
+            textColor: Theme.of(context).primaryColor,
+            onPressed: onCancel != null ? onCancel() : () => Navigator.of(context).pop(false),
+          ),
+          FlatButton(
+            child: Text("Confirmar"),
+            textColor: Theme.of(context).primaryColor,
+            onPressed: onConfirm != null ? onConfirm() : () => Navigator.of(context).pop(true),
+          )
+        ],
+      );
+    },
   );
 }
