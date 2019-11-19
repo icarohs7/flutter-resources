49dc7963288c06cd1e2b6cbfd1048c8d36055adb
Icaro Temponi <icarohs7@gmail.com>
2019-11-19 09:22:33 -0300

    Add maskedinput_resources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ maskedinput_resources/lib/maskedinput_resources.dart
@@ -0,0 +1,5 @@
+library maskedinput_resources;
+
+export 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+export 'src/field_masks.dart';
---
+++ maskedinput_resources/lib/src/field_masks.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:maskedinput_resources/maskedinput_resources.dart';
+
+const FieldMasks = _FieldMasks();
+
+@immutable
+class _FieldMasks {
+  const _FieldMasks();
+
+  MaskTextInputFormatter phone() => MaskTextInputFormatter(mask: "(##) #####-####");
+}
