bf520df32091d3a8230eb2f0d7a7e0fba1640720
Icaro Temponi <icarohs7@gmail.com>
2019-11-19 17:18:02 -0300

    Add innerFilter and innerFirst extension of Stream<List<T>>
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ core_resources/lib/src/extensions/stream_extensions.dart
@@ -0,0 +1,12 @@
+extension ListStreamExtensions<T> on Stream<List<T>> {
+  ///Apply the given filter to each list emitted by the stream
+  Stream<List<T>> innerFilter(bool Function(T element) predicate) {
+    return map((list) => list.where(predicate).toList());
+  }
+
+  ///Map each emitted list to the first item matching the given
+  ///[predicate]
+  Stream<T> innerFirst(bool Function(T element) predicate) {
+    return map((list) => list.firstWhere(predicate, orElse: () => null));
+  }
+}
