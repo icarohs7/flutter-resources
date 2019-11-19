d46370da033345f1eb328f7e013651956a34213b
Icaro Temponi <icarohs7@gmail.com>
2019-11-19 09:22:16 -0300

    Add rest_resources module and include it on basic_resource_pack
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- basic_resource_pack/lib/basic_resource_pack.dart
+++ basic_resource_pack/lib/basic_resource_pack.dart
@@ -1,2 +1,3 @@
 export 'package:bloc_resources/bloc_resources.dart';
 export 'package:navigation_resources/navigation_resources.dart';
+export 'package:rest_resources/rest_resources.dart';
---
+++ rest_resources/lib/rest_resources.dart
@@ -0,0 +1,4 @@
+library rest_resources;
+
+export 'package:dio/dio.dart';
+export 'package:dio_flutter_transformer/dio_flutter_transformer.dart';
