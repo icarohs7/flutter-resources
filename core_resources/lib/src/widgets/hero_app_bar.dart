4d14c4db023e3a6f66023fe891923dc690e16c29
Icaro Temponi <icarohs7@gmail.com>
2019-11-20 16:37:33 -0300

    Add bottom parameter to HeroAppBar
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/widgets/hero_app_bar.dart
+++ core_resources/lib/src/widgets/hero_app_bar.dart
@@ -5,12 +5,14 @@ class HeroAppBar extends PreferredSize {
     String heroTag = "toolbar",
     @required Widget title,
     List<Widget> actions,
+    PreferredSizeWidget bottom,
   }) : super(
-          preferredSize: Size.fromHeight(kToolbarHeight),
+          preferredSize: Size.fromHeight(kToolbarHeight + (bottom?.preferredSize?.height ?? 0.0)),
           child: _createAppBar(
             heroTag,
             title,
             actions,
+            bottom,
           ),
         );
 
@@ -18,11 +20,13 @@ class HeroAppBar extends PreferredSize {
     String heroTag,
     Widget title,
     List<Widget> actions,
+    PreferredSizeWidget bottom,
   ) {
     final appBar = AppBar(
       title: title,
       centerTitle: true,
       actions: actions,
+      bottom: bottom,
     );
     return heroTag != null ? Hero(tag: heroTag, child: appBar) : appBar;
   }
