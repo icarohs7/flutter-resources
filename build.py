2fa2ebf40248bc43ee7be0916eb881cc61f5d936
Icaro Temponi <icarohs7@gmail.com>
2019-11-26 16:19:23 -0300

    Add buildscript
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ build.py
@@ -0,0 +1,35 @@
+import os
+from os.path import join
+
+subdirs = next(os.walk('.'))[1]
+subdirs.remove(".git")
+subdirs.remove(".github")
+
+errs = 0
+for subdir in subdirs:
+    try:
+        commands = [
+            f"cd ./{subdir}",
+            "flutter packages get",
+            "flutter analyze",
+            "flutter test"
+        ]
+        command = "&&".join(commands)
+        result = os.system(command)
+        if result != 0:
+            raise RuntimeError()
+        print(f"{subdir} built successfully")
+        print("=================================================\n")
+    except:
+        errs += 1
+        print(f"{subdir} failed to build")
+        print("=================================================\n")
+
+module_count = len(subdirs)
+print("=================================================\n")
+print(f"{module_count - errs}/{module_count} modules succeeded building")
+print("=================================================\n")
+if os.name == "nt":
+    os.system("pause")
+if errs > 0:
+    raise RuntimeError()
