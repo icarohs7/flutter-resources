74bd0796ff7ecfc15000ad11430ac4af8828ead5
Icaro Temponi <icarohs7@gmail.com>
2019-11-28 14:10:11 -0300

    Add isLoadingStream parameter to LoadingRaisedButton
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/widgets/loading_raised_button.dart
+++ core_resources/lib/src/widgets/loading_raised_button.dart
@@ -5,19 +5,49 @@ class LoadingRaisedButton extends StatelessWidget {
     @required this.onPressed,
     this.child,
     this.isLoading,
-  });
+    this.isLoadingStream,
+  }) : assert(!(isLoading != null && isLoadingStream != null));
 
   final Widget child;
   final void Function() onPressed;
   final bool isLoading;
+  final Stream<bool> isLoadingStream;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoadingStream != null
+        ? StreamBuilder(
+            stream: isLoadingStream,
+            initialData: false,
+            builder: (context, snapshot) {
+              return _Button(isLoading: snapshot.data, onPressed: onPressed, child: child);
+            },
+          )
+        : _Button(isLoading: isLoading ?? false, onPressed: onPressed, child: child);
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({
+    Key key,
+    @required this.isLoading,
+    @required this.onPressed,
+    @required this.child,
+  }) : super(key: key);
+
+  final bool isLoading;
+  final void Function() onPressed;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return AbsorbPointer(
       absorbing: isLoading,
-      child: _Button(
+      child: RaisedButton(
         onPressed: onPressed,
         child: AnimatedSwitcher(
+          switchInCurve: Curves.easeOut,
+          switchOutCurve: Curves.easeIn,
           transitionBuilder: (child, animation) => ScaleTransition(scale: animation, child: child),
           duration: Duration(milliseconds: 300),
           child: isLoading
@@ -40,17 +70,5 @@ class LoadingRaisedButton extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class _Button extends StatelessWidget {
-  const _Button({@required this.onPressed, @required this.child});
-
-  final Widget child;
-  final void Function() onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(onPressed: onPressed, child: child);
   }
 }
