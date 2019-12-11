7cd038e5d152b2c549cc92c863b4074ebb59a55e
Icaro Temponi <icarohs7@gmail.com>
2019-12-11 17:00:20 -0300

    Update dart version to 2.7.0
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- bloc_resources/lib/bloc_resources.dart
+++ bloc_resources/lib/bloc_resources.dart
@@ -1,6 +1,7 @@
 library bloc_resources;
 
 export 'package:bloc_pattern/bloc_pattern.dart';
+export 'package:bloc_pattern/bloc_pattern_test.dart';
 export 'package:core_resources/core_resources.dart';
 export 'package:rxdart/rxdart.dart';
 
