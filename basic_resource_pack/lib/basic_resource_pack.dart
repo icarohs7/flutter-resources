49bc4fdffb8a1093bb784f2d65d78749b98dcf5f
Icaro Temponi <icarohs7@gmail.com>
2020-01-06 14:35:22 -0300

    Remove navigation_resources and basic_resource_pack modules
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- basic_resource_pack/lib/basic_resource_pack.dart
+++
@@ -1,3 +0,0 @@
-export 'package:bloc_resources/bloc_resources.dart';
-export 'package:navigation_resources/navigation_resources.dart';
-export 'package:rest_resources/rest_resources.dart';
--- navigation_resources/lib/navigation_resources.dart
+++
@@ -1,8 +0,0 @@
-library navigation_resources;
-
-export 'package:core_resources/core_resources.dart';
-export 'package:fluro/fluro.dart';
-
-export 'src/baseclasses/nx_router_base.dart';
-export 'src/fade_page_route.dart';
-export 'src/router_extensions.dart';
