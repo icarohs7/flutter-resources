e519aee4258024081d9b7d3e4ca8ce33a2639134
Icaro Temponi <icarohs7@gmail.com>
2020-01-20 15:10:41 -0300

    Add createMaterialColor top level function
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/utils/ui.dart
+++ core_resources/lib/src/utils/ui.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> showSystemOverlays() async {
@@ -9,3 +10,24 @@ Future<void> hideTopSystemOverlay() async {
 }
 
 Future<void> hideSystemOverlays() async => await SystemChrome.setEnabledSystemUIOverlays([]);
+
+///Create a material swatch with variations of
+///a base color, credits to [Filip Veličković](https://medium.com/@filipvk/creating-a-custom-color-swatch-in-flutter-554bcdcb27f3)
+MaterialColor createMaterialColor(Color color) {
+  final strengths = <double>[.05];
+  final swatch = <int, Color>{};
+  final r = color.red, g = color.green, b = color.blue;
+
+  for (var i = 1; i < 10; i++) strengths.add(0.1 * i);
+
+  for (final strength in strengths) {
+    final ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  return MaterialColor(color.value, swatch);
+}
