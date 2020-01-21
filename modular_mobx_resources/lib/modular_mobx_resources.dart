a26466097a81635b1648d99896e3d05814161bb1
Icaro Temponi <icarohs7@gmail.com>
2020-01-21 10:06:19 -0300

    Add modular_mobx_resources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ modular_mobx_resources/lib/modular_mobx_resources.dart
@@ -0,0 +1,5 @@
+library modular_mobx_resources;
+
+export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:mobx/mobx.dart' hide version;
