d1a383d5756a9d8ec8f6edb502c3d6344864c958
Icaro Temponi <icarohs7@gmail.com>
2020-01-31 10:58:48 -0300

    Add and export flutter_localizations package
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/core_resources.dart
+++ core_resources/lib/core_resources.dart
@@ -2,6 +2,7 @@ library core_resources;
 
 export 'package:async/async.dart';
 export 'package:dartx/dartx.dart';
+export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:intl/date_symbol_data_local.dart';
 export 'package:intl/intl.dart';
 export 'package:rxdart/rxdart.dart';
