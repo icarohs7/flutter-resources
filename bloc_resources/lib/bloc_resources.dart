5dd8e3809daa6223324034a70c4b515335bf3e8e
Icaro Temponi <icarohs7@gmail.com>
2020-02-11 15:16:36 -0300

    Remove bloc_resources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- bloc_resources/lib/bloc_resources.dart
+++
@@ -1,8 +0,0 @@
-library bloc_resources;
-
-export 'package:bloc_pattern/bloc_pattern.dart';
-export 'package:bloc_pattern/bloc_pattern_test.dart';
-export 'package:core_resources/core_resources.dart';
-
-export 'src/baseclasses/nx_bloc_base.dart';
-export 'src/baseclasses/nx_repository_base.dart';
