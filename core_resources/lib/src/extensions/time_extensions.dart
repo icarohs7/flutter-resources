b215589ca388a3e5378717c829d2769c23f8ef1f
Icaro Temponi <icarohs7@gmail.com>
2020-02-14 16:17:50 -0300

    Add update extension of DateTime
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/time_extensions.dart
+++ core_resources/lib/src/extensions/time_extensions.dart
@@ -1,8 +1,31 @@
-import 'package:core_resources/core_resources.dart';
+import 'package:intl/intl.dart';
 
 extension TimeExtensions on DateTime {
   ///Convert the given DateTime object to
   ///string using the given [format]
   ///and [locale]
   String string([String format, String locale]) => DateFormat(format, locale).format(this);
+
+  ///Modify the value of the given DateTime object
+  DateTime update({
+    int year,
+    int month,
+    int day,
+    int hour,
+    int minute,
+    int second,
+    int millisecond,
+    int microsecond,
+  }) {
+    return DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+      millisecond ?? this.millisecond,
+      microsecond ?? this.microsecond,
+    );
+  }
 }
--- core_resources/test/extensions/time_extensions_test.dart
+++ core_resources/test/extensions/time_extensions_test.dart
@@ -9,4 +9,30 @@ void main() {
     expect(dt.string('yyyy-MM-dd HH:mm'), equals('2020-05-09 12:00'));
     expect(dt.string('yyyy-MM-dd HH:mm:ss'), equals('2020-05-09 12:00:00'));
   });
+
+  test('should change values on DateTime', () {
+    final dtex = DateTime.parse('2020-01-01 12:00');
+    expect(dtex.year, equals(2020));
+    expect(dtex.month, equals(1));
+    expect(dtex.day, equals(1));
+    expect(dtex.hour, equals(12));
+    expect(dtex.minute, equals(0));
+    expect(dtex.second, equals(0));
+
+    final dt1 = dtex.update(hour: 14);
+    expect(dt1.year, equals(2020));
+    expect(dt1.month, equals(1));
+    expect(dt1.day, equals(1));
+    expect(dt1.hour, equals(14));
+    expect(dt1.minute, equals(0));
+    expect(dt1.second, equals(0));
+
+    final dt2 = dtex.update(hour: 15, minute: 30);
+    expect(dt2.year, equals(2020));
+    expect(dt2.month, equals(1));
+    expect(dt2.day, equals(1));
+    expect(dt2.hour, equals(15));
+    expect(dt2.minute, equals(30));
+    expect(dt2.second, equals(0));
+  });
 }
