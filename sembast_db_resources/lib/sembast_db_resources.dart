1d2f29be5b0bdc9dd2c6583f38cb81186e1921e2
Icaro Temponi <icarohs7@gmail.com>
2020-02-21 10:55:12 -0300

    Remove sembast_db_resources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- sembast_db_resources/lib/sembast_db_resources.dart
+++
@@ -1,9 +0,0 @@
-library sembast_db_resources;
-
-export 'package:abstract_db_resources/abstract_db_resources.dart';
-export 'package:path/path.dart';
-export 'package:path_provider/path_provider.dart';
-export 'package:sembast/sembast.dart';
-export 'package:sembast/sembast_io.dart';
-
-export 'src/database_extensions.dart';
