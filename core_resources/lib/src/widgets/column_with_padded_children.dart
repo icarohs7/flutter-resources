b957a370fadb5b6b930b153db85a7fe10b04321a
Icaro Temponi <icarohs7@gmail.com>
2020-03-25 16:05:48 -0300

    Add ColumnWithPaddedChildren and ListViewWithPaddedChildren
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ core_resources/lib/src/widgets/column_with_padded_children.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// A [Column] where a padding is applied to each
+/// child
+class ColumnWithPaddedChildren extends StatelessWidget {
+  const ColumnWithPaddedChildren({
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.textDirection,
+    this.verticalDirection = VerticalDirection.down,
+    this.textBaseline,
+    this.children = const <Widget>[],
+    this.childrenPadding = EdgeInsets.zero,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextDirection textDirection;
+  final VerticalDirection verticalDirection;
+  final TextBaseline textBaseline;
+  final List<Widget> children;
+  final EdgeInsets childrenPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: mainAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      crossAxisAlignment: crossAxisAlignment,
+      textDirection: textDirection,
+      verticalDirection: verticalDirection,
+      textBaseline: textBaseline,
+      children: children.map<Widget>((w) => Padding(padding: childrenPadding, child: w)).toList(),
+    );
+  }
+}
