76542cfb08e4bd64d53fe375f18778cdc8a66346
Icaro Temponi <icarohs7@gmail.com>
2020-04-20 13:09:18 -0300

    Export http_parser package from rest_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- rest_resources/lib/rest_resources.dart
+++ rest_resources/lib/rest_resources.dart
@@ -2,5 +2,6 @@ library rest_resources;
 
 export 'package:dio/dio.dart';
 export 'package:dio_flutter_transformer/dio_flutter_transformer.dart';
+export 'package:http_parser/http_parser.dart';
 
 export 'src/dio.dart';
