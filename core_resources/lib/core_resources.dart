2ba8de16376c648de2ae0c5637db93c4790a2d20
Icaro Temponi <icarohs7@gmail.com>
2020-07-13 10:11:21 -0300

    Add and export build_context package from core_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/core_resources.dart
+++ core_resources/lib/core_resources.dart
@@ -1,6 +1,7 @@
 library core_resources;
 
 export 'package:async/async.dart';
+export 'package:build_context/build_context.dart';
 export 'package:dartx/dartx.dart' hide Function0, Function1, Function2, Function3, Function4;
 export 'package:dartz/dartz.dart' hide State, Bind, Order;
 export 'package:enhanced_future_builder/enhanced_future_builder.dart';
