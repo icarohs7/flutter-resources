02f65e30412d3fa784e22ddad477b43a2e093937
Icaro Temponi <icarohs7@gmail.com>
2020-07-13 10:43:17 -0300

    Upgrade dependencies
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- modular_mobx_resources/lib/modular_mobx_resources.dart
+++ modular_mobx_resources/lib/modular_mobx_resources.dart
@@ -1,7 +1,7 @@
 library modular_mobx_resources;
 
 export 'package:flutter_mobx/flutter_mobx.dart';
-export 'package:flutter_modular/flutter_modular.dart';
+export 'package:flutter_modular/flutter_modular.dart' hide Default;
 export 'package:mobx/mobx.dart' hide version;
 
 export 'src/base_modular_router.dart';
