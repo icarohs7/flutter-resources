528c2bdb3480d97d8c109bc0405261db14fb4e86
Icaro Temponi <icarohs7@gmail.com>
2021-02-17 13:11:44 -0300

    Add truncateTo extension of String
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/string_extensions.dart
+++ core_resources/lib/src/extensions/string_extensions.dart
@@ -12,4 +12,16 @@ extension StringExtensions on String {
   T ifEmpty<T>(T Function() defaultValue) {
     return isEmpty ? defaultValue() : this;
   }
+
+  /// Returns the string up to the first
+  /// [maxLength] characters, replacing any
+  /// extra characters with an elipsis if
+  /// the parameter is true
+  String truncateTo(int maxLength, {bool elipsis = false}) {
+    return (length <= maxLength)
+        ? this
+        : elipsis
+            ? '${substring(0, maxLength)}...'
+            : substring(0, maxLength);
+  }
 }
--- core_resources/test/extensions/string_extensions_test.dart
+++ core_resources/test/extensions/string_extensions_test.dart
@@ -21,4 +21,16 @@ void main() {
     expect(s2, equals('string two'));
     expect(s3, equals('Omai wa mou shindeiru!'));
   });
+
+  test('should truncate string', () {
+    const s1 = 'omai wa mou shindeiru';
+    final s1t = s1.truncateTo(5);
+    expect(s1t, equals('omai '));
+    expect(s1t.length, equals(5));
+
+    const s2 = 'nani!?';
+    final s2t = s2.truncateTo(3, elipsis: true);
+    expect(s2t, equals('nan...'));
+    expect(s2t.length, equals(3 + 3));
+  });
 }
