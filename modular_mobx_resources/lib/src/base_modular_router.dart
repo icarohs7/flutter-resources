975520629ba6e3d0058baa0ba899d0fc2b07353d
Icaro Temponi <icarohs7@gmail.com>
2021-03-04 11:29:51 -0300

    Migrate modular_mobx_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- modular_mobx_resources/lib/src/base_modular_router.dart
+++ modular_mobx_resources/lib/src/base_modular_router.dart
@@ -2,24 +2,26 @@ import 'package:flutter/widgets.dart';
 import 'package:modular_mobx_resources/modular_mobx_resources.dart';
 
 mixin BaseModularRouter {
-  Future<T> go<T>(String route, {Object arguments}) {
+  Future<T?> go<T>(String route, {Object? arguments}) {
     return Modular.to.pushNamed(route, arguments: arguments);
   }
 
-  Future<T> goReplacement<T>(String route, {Object arguments}) {
+  Future<T?> goReplacement<T>(String route, {Object? arguments}) {
     return Modular.to.pushReplacementNamed(route, arguments: arguments);
   }
 
-  Future<T> goClearingBackstack<T>(String route, {Object arguments}) {
-    return Modular.to.pushNamedAndRemoveUntil(route, (r) => false, arguments: arguments);
+  Future<T?> goClearingBackstack<T>(String route, {Object? arguments}) {
+    return Modular.to
+        .pushNamedAndRemoveUntil(route, (r) => false, arguments: arguments);
   }
 
-  Future<T> goAndRemoveUntil<T>(
+  Future<T?> goAndRemoveUntil<T>(
     String route,
     bool Function(Route<dynamic> route) predicate, {
-    Object arguments,
+    Object? arguments,
   }) {
-    return Modular.to.pushNamedAndRemoveUntil(route, predicate, arguments: arguments);
+    return Modular.to
+        .pushNamedAndRemoveUntil(route, predicate, arguments: arguments);
   }
 
   List<String> get bottomNavRoutes => [];
@@ -27,8 +29,9 @@ mixin BaseModularRouter {
   /// The index of the curent route in the
   /// [bottomNavRoutes] list, or null if
   /// it isn't a registered route
-  int currentBottomNavIndex(BuildContext context) {
-    final routeName = ModalRoute.of(context).settings.name;
+  int? currentBottomNavIndex(BuildContext context) {
+    final routeName = ModalRoute.of(context)?.settings.name;
+    if (routeName == null) return null;
     final index = bottomNavRoutes.indexOf(routeName);
     return index != -1 ? index : null;
   }
