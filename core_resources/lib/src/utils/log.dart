be9b839f121d43259b2af34e889efb55af0c9c4a
Icaro Temponi <icarohs7@gmail.com>
2021-04-22 08:52:05 -0300

    Add clog top level function for logging
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ core_resources/lib/src/utils/log.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+/// Print to the standart output only if the app is
+/// not in debug mode
+void clog(Object object) {
+  if (!kDebugMode) return;
+  print(object);
+}
