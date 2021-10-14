a5513ebab678c3235e5c93e3925dbf7ce43c5439
Icaro Temponi <icarohs7@gmail.com>
2021-10-14 11:53:10 -0300

    Add firestore_db_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ firestore_db_resources/lib/firestore_db_resources.dart
@@ -0,0 +1,6 @@
+library firestore_db_resources;
+
+export 'package:abstract_stringkey_db_resources/abstract_stringkey_db_resources.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+
+export 'src/firestore_database.dart' hide FirestoreDatabase;
