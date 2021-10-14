9723cb46d0c698cc34a6c4722a127bc0d842bde5
Icaro Temponi <icarohs7@gmail.com>
2021-10-14 13:55:25 -0300

    Cleanup
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/widgets/splash_widget.dart
+++ core_resources/lib/src/widgets/splash_widget.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:core_resources/src/utils/log.dart';
 import 'package:core_resources/src/utils/ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
