2e74ee8d5fa1a731fbb3307875bdbd223ed68f21
Icaro Temponi <icarohs7@gmail.com>
2022-02-16 14:34:34 -0300

    Add obsFMap extension of Stream<Map<K, V>>
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- getx_core_resources/lib/src/extensions/stream_extensions.dart
+++ getx_core_resources/lib/src/extensions/stream_extensions.dart
@@ -1,8 +1,16 @@
 import 'package:getx_core_resources/getx_core_resources.dart';
 
+/// GCR = Getx Core Resources
 extension GCRStreamExtensions<T> on Stream<T> {
   /// Create an observable from a stream,
   /// containing a null value initially
   /// and updating according to the stream events
   Rxn<T> obsF({T? initialValue}) => Rxn<T>(initialValue)..bindStream(this);
+}
+
+extension GCRStreamOfMapExtensions<K, V> on Stream<Map<K, V>> {
+  /// Create an observable map from a stream,
+  /// containing an empty collection initially
+  /// and updating according to the stream events
+  RxMap<K, V> obsFMap({Map<K, V> initialValue = const {}}) => RxMap(initialValue)..bindStream(this);
 }
