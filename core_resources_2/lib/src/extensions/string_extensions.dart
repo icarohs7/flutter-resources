b00173c1db5fa77500b96a473c5d5da7f304e66c
Icaro Temponi <icarohs7@gmail.com>
2022-03-21 13:41:29 -0300

    Remove core_resources_2 and modular_mobx_resources modules
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources_2/lib/src/extensions/string_extensions.dart
+++
@@ -1,7 +0,0 @@
-import 'package:html_unescape/html_unescape.dart';
-
-final _htmlUnescape = HtmlUnescape();
-
-extension CR2StringEx on String {
-  String get htmlUnescaped => _htmlUnescape.convert(this);
-}
--- modular_mobx_resources/lib/modular_mobx_resources.dart
+++
@@ -1,9 +0,0 @@
-library modular_mobx_resources;
-
-export 'package:flutter_mobx/flutter_mobx.dart';
-export 'package:flutter_modular/flutter_modular.dart' hide Default;
-export 'package:mobx/mobx.dart' hide version, ObservableStreamExtension;
-
-export 'src/base_modular_router.dart';
-export 'src/mobx_extensions.dart';
-export 'src/top_level.dart';
