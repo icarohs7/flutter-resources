001a376421ca744d21f50ed8687c8ea000ce1988
Icaro Temponi <icarohs7@gmail.com>
2022-05-05 10:47:22 -0300

    Allow null on function parameter
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/color_extensions.dart
+++ core_resources/lib/src/extensions/color_extensions.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 
 extension ColorExtensions on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
-  static Color fromHex(String hexString) {
+  static Color fromHex(String? hexString) {
+    if (hexString == null) return Colors.black;
     final buffer = StringBuffer();
     if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
     buffer.write(hexString.replaceFirst('#', ''));
