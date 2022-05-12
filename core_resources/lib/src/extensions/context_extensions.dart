8651370d9423a8b8c2e7e2e5bbddc0a2242a9f6d
Icaro Temponi <icarohs7@gmail.com>
2022-05-12 14:21:15 -0300

    Add isSystemDarkTheme BuildContext extension
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/context_extensions.dart
+++ core_resources/lib/src/extensions/context_extensions.dart
@@ -1,3 +1,4 @@
+import 'package:build_context/build_context.dart';
 import 'package:flutter/material.dart';
 
 extension CRContextExtensions on BuildContext {
@@ -6,4 +7,6 @@ extension CRContextExtensions on BuildContext {
   ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackbar(SnackBar snackBar) {
     return scaffoldMessenger.showSnackBar(snackBar);
   }
+
+  bool get isSystemDarkTheme => platformBrightness == Brightness.dark;
 }
