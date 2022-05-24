b105c40812e4157a4768a48131a13558d59c91ae
Icaro Temponi <icarohs7@gmail.com>
2022-05-24 12:57:18 -0300

    Add toStringTrimmingDecimals extension of double
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/number_extensions.dart
+++ core_resources/lib/src/extensions/number_extensions.dart
@@ -11,4 +11,10 @@ extension StringToNumberExtensions on String {
 extension DoubleExtensions on double {
   ///Convert the given value to a currency representation
   String asCurrency() => 'R\$ ' + toStringAsFixed(2).replaceAll('.', ',');
+
+  ///Convert the given value to a string representation,
+  ///removing trailing zeros in the decimal part
+  String toStringTrimmingDecimals({int precision = 5}) {
+    return toStringAsFixed(precision).replaceAll(RegExp(r'([.]*0+)(?!.*\d)'), '');
+  }
 }
--- core_resources/test/extensions/number_extensions_test.dart
+++ core_resources/test/extensions/number_extensions_test.dart
@@ -16,4 +16,12 @@ void main() {
     expect(15.32.asCurrency(), r'R$ 15,32');
     expect(1000.99.asCurrency(), r'R$ 1000,99');
   });
+
+  test('should remove trailing zeros from double number', () {
+    expect(15.32.toStringTrimmingDecimals(), '15.32');
+    expect(15.3.toStringTrimmingDecimals(), '15.3');
+    expect(15.392.toStringTrimmingDecimals(precision: 2), '15.39');
+    expect(4.0.toStringTrimmingDecimals(precision: 2), '4');
+    expect(4.2000000.toStringTrimmingDecimals(precision: 2), '4.2');
+  });
 }
