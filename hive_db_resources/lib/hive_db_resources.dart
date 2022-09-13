a28bd61f4fb6cbc99d79df26266b54cd67a2c0ac
Icaro Temponi <icarohs7@gmail.com>
2022-09-13 10:56:22 -0300

    Remove hive_db_resources module
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- hive_db_resources/lib/hive_db_resources.dart
+++
@@ -1,7 +0,0 @@
-library hive_db_resources;
-
-export 'package:abstract_db_resources/abstract_db_resources.dart';
-export 'package:hive_flutter/hive_flutter.dart';
-
-export 'src/hive_database.dart' hide HiveDatabase;
-export 'src/hive_db_resources.dart';
--- hive_db_resources/lib/src/hive_db_resources.dart
+++
@@ -1,9 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-// ignore: avoid_classes_with_only_static_members
-class HiveDbResources {
-  static Future<void> init() async {
-    await Hive.initFlutter();
-    await Hive.openBox<String>('globalBox');
-  }
-}
