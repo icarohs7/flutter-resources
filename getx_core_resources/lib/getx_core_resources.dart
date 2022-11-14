fa3d4cd1e0110e0e261a48bb5a26bef06e06050e
Icaro Temponi <icarohs7@gmail.com>
2022-11-14 16:33:30 -0300

    Remove shared dependencies, replace HiveDatabase from core_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- getx_core_resources/lib/getx_core_resources.dart
+++ getx_core_resources/lib/getx_core_resources.dart
@@ -1,6 +1,9 @@
 library getx_core_resources;
 
-export 'package:core_resources/core_resources.dart';
+import 'package:core_resources/core_resources.dart';
+import 'package:get/get.dart';
+
+export 'package:core_resources/core_resources.dart' hide HiveDbResources;
 export 'package:dio/dio.dart';
 export 'package:get/get.dart'
     hide ListExtension, FormData, MultipartFile, Response, ContextExtensionss;
@@ -9,20 +12,15 @@ export 'package:http_parser/http_parser.dart';
 export 'package:rxdart/rxdart.dart' hide Rx;
 
 export 'src/base_get_router.dart';
-export 'src/baseclasses/abstract_json_database.dart';
-export 'src/baseclasses/abstract_t_database.dart';
-export 'src/baseclasses/base_api_service.dart';
-export 'src/baseclasses/base_getx_module.dart';
-export 'src/baseclasses/base_repository.dart';
 export 'src/extensions/future_extensions.dart';
 export 'src/extensions/getx_extensions.dart';
 export 'src/extensions/stream_extensions.dart';
-export 'src/extensions/string_extensions.dart';
-export 'src/hive_database.dart' hide HiveDatabase;
-export 'src/hive_db_resources.dart';
-export 'src/utils/common_intents.dart';
-export 'src/utils/dio.dart';
-export 'src/utils/simple_search_delegate.dart';
-export 'src/widgets/auto_complete_text_view.dart';
-export 'src/widgets/editable_label.dart';
-export 'src/widgets/password_form_field.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class HiveDbResources {
+  static Future<void> init() async {
+    Core.setLocator(Get.find);
+    await Hive.initFlutter();
+    await Hive.openBox<String>('globalBox');
+  }
+}
--- getx_core_resources/lib/src/baseclasses/base_getx_module.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class BaseGetxModule {
-  List<GetPage> get pages => [];
-
-  Bindings get binding => BindingsBuilder(() {});
-}
