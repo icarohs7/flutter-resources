9e913bb7ba597211c6e9c73eabaad43c65cc35be
Icaro Temponi <icarohs7@gmail.com>
2022-12-03 12:51:13 -0300

    Add subject extension of Object
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/object_extensions.dart
+++ core_resources/lib/src/extensions/object_extensions.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 extension ObjectExtensions<T> on T {
   MaterialStateProperty<T> get materialProperty => MaterialStateProperty.all(this);
+
+  BehaviorSubject<T> get subject => BehaviorSubject.seeded(this);
 }
