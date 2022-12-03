14fda2b7c7c19e5a016c2cce11464c5824ba5035
Icaro Temponi <icarohs7@gmail.com>
2022-12-03 12:19:16 -0300

    Export getIt variable form getit_core_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- getit_core_resources/lib/getit_core_resources.dart
+++ getit_core_resources/lib/getit_core_resources.dart
@@ -5,6 +5,8 @@ import 'getit_core_resources.dart';
 export 'package:core_resources/core_resources.dart' hide HiveDbResources;
 export 'package:get_it/get_it.dart';
 
+final getIt = GetIt.instance;
+
 // ignore: avoid_classes_with_only_static_members
 class HiveDbResources {
   static Future<void> init() async {
