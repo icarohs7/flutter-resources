2e9f75737b9e1b0c026e95032029e751afee278c
Icaro Temponi <icarohs7@gmail.com>
2022-12-04 12:23:45 -0300

    Add collect and collectValue extensions of Stream
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/stream_extensions.dart
+++ core_resources/lib/src/extensions/stream_extensions.dart
@@ -1,5 +1,7 @@
 import 'package:collection/collection.dart' show IterableExtension;
 
+import '../../core_resources.dart';
+
 extension ListStreamExtensions<T> on Stream<List<T>> {
   ///Apply the given filter to each list emitted by the stream
   Stream<List<T>> innerFilter(bool Function(T element) predicate) {
@@ -11,4 +13,19 @@ extension ListStreamExtensions<T> on Stream<List<T>> {
   Stream<T?> innerFirst(bool Function(T element) predicate) {
     return map((list) => list.firstWhereOrNull(predicate));
   }
+}
+
+extension CoreStreamExtensions<T> on Stream<T> {
+  /// Convert each emission into a list aggregating
+  /// all previous emissions plus the latest one
+  Stream<List<T>> collect() async* {
+    final items = <T>[];
+    await for (final item in this) {
+      items.add(item);
+      yield items;
+    }
+  }
+
+  /// [collect] with a initial value
+  ValueStream<List<T>> collectValue() => collect().shareValueSeeded([]);
 }
