147beb5a0376d243ffe6637959de6f2d87c3ce3d
Icaro Temponi <icarohs7@gmail.com>
2023-01-05 17:43:51 -0300

    Remove extensions
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/extensions/stream_extensions.dart
+++ core_resources/lib/src/extensions/stream_extensions.dart
@@ -1,7 +1,5 @@
 import 'package:collection/collection.dart' show IterableExtension;
 
-import '../../core_resources.dart';
-
 extension ListStreamExtensions<T> on Stream<List<T>> {
   ///Apply the given filter to each list emitted by the stream
   Stream<List<T>> innerFilter(bool Function(T element) predicate) {
@@ -13,19 +11,4 @@ extension ListStreamExtensions<T> on Stream<List<T>> {
   Stream<T?> innerFirst(bool Function(T element) predicate) {
     return map((list) => list.firstWhereOrNull(predicate));
   }
-}
-
-extension CoreStreamExtensions<T> on Stream<T> {
-  /// Convert each emission into a list aggregating
-  /// all previous emissions plus the latest one
-  Stream<List<T>> collect() async* {
-    final items = <T>[];
-    await for (final item in this) {
-      items.add(item);
-      yield items;
-    }
-  }
-
-  /// [collect] with a initial value
-  ValueStream<List<T>> collectValue() => collect().shareValueSeeded([]);
 }
--- core_resources/test/extensions/stream_extensions_test.dart
+++ core_resources/test/extensions/stream_extensions_test.dart
@@ -21,26 +21,4 @@ void main() {
     final f2 = s2.innerFirst((e) => e.startsWith('s'));
     expect(await f2.first, 'shindeiru!');
   });
-
-  test('should collect stream into list', () async {
-    final s1 = Stream.fromIterable([1, 2, 3, 4]);
-    final f1 = s1.collect();
-    expect(await f1.last, [1, 2, 3, 4]);
-
-    final s2 = Stream.fromIterable(['Omae wa', 'mou', 'shindeiru!']);
-    final f2 = s2.collect();
-    expect(await f2.last, ['Omae wa', 'mou', 'shindeiru!']);
-  });
-
-  test('should collect stream into list with initial value', () async {
-    final s1 = Stream.fromIterable([1, 2, 3, 4]);
-    final f1 = s1.collectValue();
-    expect(f1.value, equals(<int>[]));
-    expect(await f1.last, [1, 2, 3, 4]);
-
-    final s2 = Stream.fromIterable(['Omae wa', 'mou', 'shindeiru!']);
-    final f2 = s2.collectValue();
-    expect(f2.value, equals(<String>[]));
-    expect(await f2.last, ['Omae wa', 'mou', 'shindeiru!']);
-  });
 }
