60d96d89c8ec9e8c5de4acd6312d4f8fc7056b32
Icaro Temponi <icarohs7@gmail.com>
2023-02-05 20:18:09 -0300

    Add name to function parameter
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- value_notifier_resources/lib/src/extensions/value_listenable_extensions.dart
+++ value_notifier_resources/lib/src/extensions/value_listenable_extensions.dart
@@ -7,12 +7,12 @@ extension CRValueListenableExtensions<T> on ValueListenable<T> {
   /// `ValueListenable<T>`
   /// on each value change of `this` the conversion function
   /// [convert] is called to do the type conversion
-  ValueListenable<R> mapEvent<R>(R Function(T) convert) => map(convert);
+  ValueListenable<R> mapEvent<R>(R Function(T value) convert) => map(convert);
 
   ///
   /// [whereEvent] allows you to set a filter on a `ValueListenable` so that an installed
   /// handler function is only called if the passed
   /// [selector] function returns true. Because the selector function is called on
   /// every new value you can change the filter during runtime.
-  ValueListenable<T> whereEvent(bool Function(T) selector) => where(selector);
+  ValueListenable<T> whereEvent(bool Function(T value) selector) => where(selector);
 }
