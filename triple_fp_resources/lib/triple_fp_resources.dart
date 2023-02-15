03bd6f55b9aae7daa221cd697d8be3da88a1884b
Icaro Temponi <icarohs7@gmail.com>
2023-02-15 15:36:30 -0300

    Add and export fpdart dependency to triple_fp_resources
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- triple_fp_resources/lib/triple_fp_resources.dart
+++ triple_fp_resources/lib/triple_fp_resources.dart
@@ -1,6 +1,7 @@
 library triple_fp_resources;
 
 export 'package:flutter_triple/flutter_triple.dart';
+export 'package:fpdart/fpdart.dart';
 
 export 'src/disposable.dart';
 export 'src/reactor.dart';
