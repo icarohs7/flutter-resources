fc456019b112ce834914561afbcdf346d29d0117
Icaro Temponi <icarohs7@gmail.com>
2023-02-15 14:08:24 -0300

    Export Disposable
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- triple_fp_resources/lib/triple_fp_resources.dart
+++ triple_fp_resources/lib/triple_fp_resources.dart
@@ -2,6 +2,7 @@ library triple_fp_resources;
 
 export 'package:flutter_triple/flutter_triple.dart';
 
+export 'src/disposable.dart';
 export 'src/reactor.dart';
 export 'src/reactor_store.dart';
 export 'src/stream_reactor.dart';
