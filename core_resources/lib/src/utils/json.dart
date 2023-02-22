f4cceb8729399454fb79642af6792913cdf2a255
Icaro Temponi <icarohs7@gmail.com>
2023-02-22 13:07:16 -0300

    Use compute instead of Isolate.run
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/utils/json.dart
+++ core_resources/lib/src/utils/json.dart
@@ -1,22 +1,21 @@
 import 'dart:convert';
-import 'dart:isolate';
 
 import 'package:flutter/foundation.dart';
 
 /// Decode a String to a JSON object on background
 /// using the [compute] function
 Future<Map<String, dynamic>> jsonDecodeObj(String source) async {
-  return Isolate.run(() => jsonDecode(source));
+  return compute((s) => jsonDecode(s), source);
 }
 
 /// Decode a String to a JSON array on background
 /// using the [compute] function
 Future<List<dynamic>> jsonDecodeArray(String source) async {
-  return Isolate.run(() => jsonDecode(source));
+  return compute((s) => jsonDecode(s), source);
 }
 
 /// Encode an object to its JSON representation
 /// on background using the [compute] function
 Future<String> jsonEncodeBg(dynamic obj) async {
-  return Isolate.run(() => jsonEncode(obj));
+  return compute((obs) => jsonEncode(obj), obj);
 }
