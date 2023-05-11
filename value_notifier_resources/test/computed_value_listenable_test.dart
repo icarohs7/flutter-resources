f1207032d97f5c1ebe0096cba59b47d734b9bae2
Icaro Temponi <icarohs7@gmail.com>
2023-05-11 15:05:06 -0300

    Remove flutter/cupertino usages
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- value_notifier_resources/test/computed_value_listenable_test.dart
+++ value_notifier_resources/test/computed_value_listenable_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:value_notifier_resources/value_notifier_resources.dart';
 
