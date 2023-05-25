f00772d0cf303555ee650037a1945f3670ef5b6c
Icaro Temponi <icarohs7@gmail.com>
2023-05-25 19:20:03 -0300

    Remove type and future parameter of SplashWidget
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/widgets/splash_widget.dart
+++ core_resources/lib/src/widgets/splash_widget.dart
@@ -5,30 +5,27 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SplashWidget<T> extends HookWidget {
+/// Returns a widget wrapping the [child] and running
+/// the given [operation] once
+class SplashWidget extends HookWidget {
   const SplashWidget({
     super.key,
-    required this.future,
     required this.child,
-    required this.onComplete,
+    required this.operation,
   });
 
-  final Future<T> future;
   final Widget child;
-  final FutureOr<void> Function(BuildContext context, T value) onComplete;
+  final FutureOr<void> Function(BuildContext context) operation;
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(() => this.future);
-    final onComplete = useMemoized(() => this.onComplete);
+    final operation = useMemoized(() => this.operation);
     final isMounted = useIsMounted();
 
     useEffect(() {
       Future(() async {
         if (!isMounted()) return;
-        final result = await future;
-        if (!isMounted()) return;
-        await onComplete(context, result);
+        await operation(context);
       });
       return null;
     }, []);
--- core_resources/test/widgets/splash_widget_test.dart
+++ core_resources/test/widgets/splash_widget_test.dart
@@ -7,16 +7,13 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('SplashWidget', (tester) async {
     final completer = Completer<int>();
-    var returned = 0;
     var result = 0;
 
     await tester.pumpWidget(
       MaterialApp(
-        home: SplashWidget<int>(
-          future: completer.future,
+        home: SplashWidget(
           child: const Text('waiting'),
-          onComplete: (context, value) async {
-            returned = value;
+          operation: (context) async {
             result++;
             context.push(MaterialPageRoute(builder: (_) => const Text('done')));
           },
@@ -25,14 +22,12 @@ void main() {
     );
 
     expect(find.text('waiting'), findsOneWidget);
-    expect(returned, 0);
     expect(result, 0);
 
     completer.complete(1);
     await tester.pumpAndSettle();
 
     expect(find.text('waiting'), findsNothing);
-    expect(returned, 1);
     expect(result, 1);
     expect(find.text('done'), findsOneWidget);
   });
