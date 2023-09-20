a964d556abe78edf9ad85dba147409b009c405b4
Icaro Temponi <icarohs7@gmail.com>
2023-09-20 10:19:18 -0300

    Remove leftover class
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- reactor_fp_resources/lib/src/exception_wrapper.dart
+++
@@ -1,9 +0,0 @@
-class ExceptionWrapper implements Exception {
-  final Object error;
-  final StackTrace? stack;
-
-  ExceptionWrapper(this.error, this.stack);
-
-  @override
-  String toString() => '$error\n$stack';
-}
