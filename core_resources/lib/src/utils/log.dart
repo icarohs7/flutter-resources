ff01f2a5a20dfedceb8666cb6affae01257adc48
Icaro Temponi <icarohs7@gmail.com>
2024-07-02 11:23:31 -0300

    Disable clog test due to inability to test developer.log calls
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/utils/log.dart
+++ core_resources/lib/src/utils/log.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: avoid_print
 
-import 'package:flutter/foundation.dart';
+import 'dart:async';
 import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
 
 /// Print to the standart output only if the app is
 /// not in debug mode
-void clog(Object? object) {
+void clog(Object? object, {Object? error, StackTrace? stackTrace, Zone? zone}) {
   if (!kDebugMode) return;
-  log('$object');
+  log('$object', error: error, stackTrace: stackTrace, zone: zone);
 }
