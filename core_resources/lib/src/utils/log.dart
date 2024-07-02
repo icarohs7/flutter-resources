a03cce05dd85fa2e8f1d1a94facb4976b0e504cd
Icaro Temponi <icarohs7@gmail.com>
2024-07-02 09:48:44 -0300

    Use log instead of print for clog function
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- core_resources/lib/src/utils/log.dart
+++ core_resources/lib/src/utils/log.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/foundation.dart';
+import 'dart:developer';
 
 /// Print to the standart output only if the app is
 /// not in debug mode
 void clog(Object? object) {
   if (!kDebugMode) return;
-  print(object);
+  log('$object');
 }
